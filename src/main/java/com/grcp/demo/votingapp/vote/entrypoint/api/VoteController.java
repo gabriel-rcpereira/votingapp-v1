f3d7754055c5dd4d1f19a05694955dde6e8644a5
gcastropereira <gabriel.pereira@waitrose.co.uk>
2024-02-22 14:31:06 -0300

    polish the code

--- src/main/java/com/grcp/demo/votingapp/vote/entrypoint/api/VoteController.java
+++ src/main/java/com/grcp/demo/votingapp/vote/entrypoint/api/VoteController.java
@@ -1,7 +1,6 @@
 package com.grcp.demo.votingapp.vote.entrypoint.api;
 
 import com.grcp.demo.votingapp.pool.domain.PoolId;
-import com.grcp.demo.votingapp.pool.domain.PoolOptionId;
 import com.grcp.demo.votingapp.vote.domain.AggregatedVotingResult;
 import com.grcp.demo.votingapp.vote.domain.Vote;
 import com.grcp.demo.votingapp.vote.entrypoint.model.AggregatedVotingResultResponseDto;
@@ -17,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.grcp.demo.votingapp.vote.entrypoint.mapper.VoteDtoMapper.toAggregatedVotingResultResponseDto;
+import static com.grcp.demo.votingapp.vote.entrypoint.mapper.VoteDtoMapper.toPoolId;
+import static com.grcp.demo.votingapp.vote.entrypoint.mapper.VoteDtoMapper.toVote;
 
 @RequiredArgsConstructor
 @Validated
@@ -26,19 +27,16 @@ public class VoteController {
     private final VoteService voteService;
 
     @PostMapping("/api/v1/pools/{poolId}/votes")
-    public ResponseEntity<?> postNewVote(
-            @PathVariable("poolId") Long poolId,
-            @RequestBody VoteRequestDto request) {
-        PoolOptionId typedPoolOptionId = new PoolOptionId(request.poolOptionId());
-        Vote newVote = new Vote(typedPoolOptionId);
-        PoolId typedPoolId = new PoolId(poolId);
+    public ResponseEntity<Void> postNewVote(@PathVariable("poolId") Long poolId, @RequestBody VoteRequestDto request) {
+        Vote newVote = toVote(request);
+        PoolId typedPoolId = toPoolId(poolId);
         voteService.registerNewVote(typedPoolId, newVote);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/api/v1/pools/{poolId}/votes")
     public ResponseEntity<AggregatedVotingResultResponseDto> getVotes(@PathVariable("poolId") Long poolId) {
-        PoolId typePoolId = new PoolId(poolId);
+        PoolId typePoolId = toPoolId(poolId);
         AggregatedVotingResult aggregatedVotingResult = voteService.fetchAggregatedVotingResult(typePoolId);
         AggregatedVotingResultResponseDto aggregatedVotingResultResponseDto = toAggregatedVotingResultResponseDto(
                 aggregatedVotingResult);
--- src/main/java/com/grcp/demo/votingapp/vote/entrypoint/mapper/VoteDtoMapper.java
+++ src/main/java/com/grcp/demo/votingapp/vote/entrypoint/mapper/VoteDtoMapper.java
@@ -1,8 +1,12 @@
 package com.grcp.demo.votingapp.vote.entrypoint.mapper;
 
+import com.grcp.demo.votingapp.pool.domain.PoolId;
+import com.grcp.demo.votingapp.pool.domain.PoolOptionId;
 import com.grcp.demo.votingapp.vote.domain.AggregatedVotingResult;
+import com.grcp.demo.votingapp.vote.domain.Vote;
 import com.grcp.demo.votingapp.vote.domain.VotingResult;
 import com.grcp.demo.votingapp.vote.entrypoint.model.AggregatedVotingResultResponseDto;
+import com.grcp.demo.votingapp.vote.entrypoint.model.VoteRequestDto;
 import com.grcp.demo.votingapp.vote.entrypoint.model.VotingResultResponseDto;
 
 import java.util.List;
@@ -14,6 +18,15 @@ public class VoteDtoMapper {
         return VoteDtoMapper.toAggregatedVotingResultResponseDto(
                 aggregatedVotingResult,
                 votingResultsResponse);
+    }
+
+    public static PoolId toPoolId(Long poolId) {
+        return new PoolId(poolId);
+    }
+
+    public static Vote toVote(VoteRequestDto request) {
+        PoolOptionId typedPoolOptionId = new PoolOptionId(request.poolOptionId());
+        return new Vote(typedPoolOptionId);
     }
 
     private static AggregatedVotingResultResponseDto toAggregatedVotingResultResponseDto(
